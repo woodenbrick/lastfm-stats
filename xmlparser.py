132e6a33a7cb6251b943b3496619fcbe9411ddce
Daniel <wodemoneke@gmail.com>
2008-12-02 17:49:12 +0200

    eod

--- xmlparser.py
+++ xmlparser.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
+import xml.etree.ElementTree as ET
 
-class Lastfm_Xml_Handler(ContentHandler):
-    
-    def __init__(self):
-        self.inTitle = 0
-        self.mapping = {}
- 
-  def startElement(self, name, attributes):
-    if name == "book":
-      self.buffer = ""
-      self.isbn = attributes["isbn"]
-    elif name == "title":
-      self.inTitle = 1
- 
-  def characters(self, data):
-    if self.inTitle:
-      self.buffer += data
- 
-  def endElement(self, name):
-    if name == "title":
-      self.inTitle = 0
-      self.mapping[self.isbn] = self.buffer
+xml = './xml/user.getrecenttracks.xml'
+doc = ET.ElementTree(xml)
+iter = doc.getiterator()
+print iter
 
-Extracting the information we're looking for is now trivial. If the code above is in bookhandler.py and our sample document is in books.xml, we could do this in an interactive session:
-
->>> import xml.sax
->>> import bookhandler
->>> import pprint
->>> 
->>> parser = xml.sax.make_parser(  )
->>> handler = bookhandler.BookHandler(  )
->>> parser.setContentHandler(handler)
->>> parser.parse("books.xml")
->>> pprint.pprint(handler.mapping)
-{u'1-56592-051-1': u'Making TeX Work',
- u'1-56592-724-9': u'The Cathedral & the Bazaar'}
+#tree = doc.getiterator(wanted)
+#for element in tree:
+#    print element.text
