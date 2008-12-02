0556742bc6093203cf6dc3eedd89aa8434f53e46
Daniel <wodemoneke@gmail.com>
2008-12-02 14:32:39 +0200

    created parser

--- lastfm-stats.py
+++ lastfm-stats.py
@@ -2,7 +2,9 @@ import webbrowser
 import md5
 import urllib
 import urllib2
-
+import time
+from errorcodes import Lastfm_Api_Error
+import xmlparser
 #currently implemented in the set_method function:
 #user.getrecenttracks
 #this may be reimplemented as a class if it becomes too unwieldy
@@ -18,20 +20,32 @@ class Lastfm_Stats:
         
     def set_method(self, method, limit=10):
         self.get_values['method'] = method
-        #self.get_values['limit'] = limit
         self.filename = self.xml_dir + method + '.xml'
 
     def encode_url_values(self):
         values = urllib.urlencode(self.get_values)
         return values
     
-    def request_data(self, values):
-        req = urllib.urlretrieve(url = self.base_url + values, filename = self.filename)
+    def request_data(self):
+        values = self.encode_url_values()
+        urllib.urlretrieve(url = self.base_url + values, filename = self.filename)
+        
+        
+    def parse_data(self):
+        """Checks that data is correct, deletes otherwise and shows error"""
+        parser = xmlparser(self.filename)
+        parser.parse()
+        
     
   
 if __name__ == '__main__':
     lastfm = Lastfm_Stats()
     lastfm.set_user('woodenbrick')
-    lastfm.set_method('user.getrecenttracks')
-    url_values = lastfm.encode_url_values()
-    request = lastfm.request_data(url_values)
+    wanted_data = ['user.getrecenttracks', 'artist.getTopAlbums', 'user.getLovedTracks']
+    for method in wanted_data:
+        lastfm.set_method(method)
+        print 'Getting', method
+        lastfm.request_data()
+        lastfm.parse_data()
+        time.sleep(1)
+    print 'Complete'
