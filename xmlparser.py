77809b98aaeb7bfdd2ebc5859953f3ea1bbe9471
Daniel <wodemoneke@gmail.com>
2008-12-03 14:21:18 +0200

    This is a functional version of the xmlparser.
    Will benow cleaned up

--- xmlparser.py
+++ xmlparser.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 import xml.etree.ElementTree as ET
-
-xml = './xml/user.getrecenttracks.xml'
-doc = ET.ElementTree(xml)
+import os
+path = os.path.dirname(os.path.realpath(__file__))
+xml = path + '/xml/user.getrecenttracks.xml'
+doc = ET.parse(xml)
 iter = doc.getiterator()
-print iter
+wanted_subs = {'date' : 'uts'}
+wanted = ['artist', 'name', 'date']
 
+collected = []
+current = {}
+for child in iter:
+    if child.tag in wanted:
+        if child.tag in wanted_subs:
+            current[child.tag] = child.attrib['uts']
+        else:
+            current[child.tag] = child.text
+    if len(wanted) == len(current):
+        collected.append(current)
+        current = {}
+print collected
 #tree = doc.getiterator(wanted)
 #for element in tree:
 #    print element.text
